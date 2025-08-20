6bd1e60517a5b55afcd58c5da880b84f6b002107
kelyzor <kelyzor25@gmail.com>
2025-08-20 20:14:33 +1000

    fix: add li tags

--- app/quiz/Questions.tsx
+++ app/quiz/Questions.tsx
@@ -2,6 +2,7 @@ import type { JSX } from "react"
 import clsx from "clsx"
 import { useQuizStore } from "@/app/store/useQuizStore";
 import { PreparedQuestion, Answer } from "@/app/types";
+import { nanoid } from "nanoid";
 
 export default function Questions(): JSX.Element[] {
     const { questions, selectedAnswers, selectAnswer, isQuizOver } = useQuizStore()
@@ -24,7 +25,14 @@ export default function Questions(): JSX.Element[] {
                         const isCorrect: boolean = a.isCorrect
                         const isSelected: boolean = selectedAnswers.some(sA => sA.buttonId === a.id)
                         const buttonStyle: string = clsx("active:scale-95 active:duration-50 rounded-lg pt-2 pb-2 pr-4 pl-4 font-medium font-inter text-[0.75em] lg:text-[0.5em]", getButtonStatus(isCorrect, isSelected), isQuizOver ? "cursor-not-allowed" : "cursor-pointer")
-                        return <button onClick={() => selectAnswer(question.sectionId, a.id, isCorrect)} disabled={isQuizOver} key={a.id} className={buttonStyle}>{a.text}</button>
+                        return <li key={nanoid()}>
+                            <button
+                                onClick={() => selectAnswer(question.sectionId, a.id, isCorrect)}
+                                disabled={isQuizOver} key={a.id} className={buttonStyle}
+                            >
+                                {a.text}
+                            </button>
+                        </li>
                     })}
                 </ul>
                 <hr />
