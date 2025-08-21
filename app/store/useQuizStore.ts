b151eff8c550bfc87109d2d73d5992e6247777fc
kelyzor <kelyzor25@gmail.com>
2025-08-21 22:08:16 +1000

    docs(store): add detailed logic comments to Zustand store

--- app/store/useQuizStore.ts
+++ app/store/useQuizStore.ts
@@ -2,10 +2,20 @@ import { create } from "zustand"
 import { QuizState } from "@/app/types"
 
 export const useQuizStore = create<QuizState>()((set) => ({
+    // Array of questions
     questions: [],
+
+    // Array of selected answers
     selectedAnswers: [],
+
+    // Boolean game over or not
     isQuizOver: false,
+
+    // Puts new questions
     setQuestions: (newQuestions) => set({ questions: newQuestions }),
+    
+    // Puts the new selected answer in a separate object if it is not in the list, 
+    // otherwise changes the existing one to the values of the new one
     selectAnswer: (sectionId, buttonId, isCorrect) => {
         set((state) => {
             const isExisting = state.selectedAnswers.find(answer => answer.sectionId === sectionId)
@@ -21,10 +31,14 @@ export const useQuizStore = create<QuizState>()((set) => ({
             }
         })
     },
-    resetGame: () => set({ 
+
+    // Reset all values
+    resetGame: () => set({
         selectedAnswers: [],
         questions: [],
         isQuizOver: false
     }),
+
+    // Changing the game status to a new one
     setIsQuizOver: (newIsQuizOver) => set({ isQuizOver: newIsQuizOver })
 }))
